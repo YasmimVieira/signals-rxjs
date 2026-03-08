1245fe8ff3979137102968cb210ee8ebe55e2ebb
YasmimVieira <vieiray937@gmail.com>
2026-03-08 14:51:16 -0300

    feat: implement counter functionality with increase, decrease, and reset methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-labs';
+    public count = signal(0)
+  public isMax = computed(() => this.count() > 10);
+
+  constructor() {
+    effect(() => {
+      console.log(`Valor atual do contador: ${this.count()}`);
+    });
+  }
+
+
+  public increaseValue(): void {
+    this.count.update((actualValue) => actualValue + 1);
+  }
+
+  public decreaseValue(): void {
+    this.count.update((actualValue) => actualValue - 1);
+  }
+
+  public resetValue(): void {
+    this.count.set(0);
+  }
 }
